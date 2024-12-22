9016af00b63a66bdebf77f8bac28e2c286254428
umer321-apex <mu0829332@gmail.com>
2024-12-22 16:19:48 +0500

    path adjusted

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,3 @@
-// const express = require("express");
-// const http = require("http");
-
-// const connectDB = require("./config/db");
-// const authRoutes = require("./routes/authRoutes");
-// const path = require("path")
-// const app = express();
-// const cors = require("cors");
-// const server = http.createServer(app);
-
-
-// connectDB();
-// app.use(cors());
-// app.use(express.json());  
-// app.use("/api", authRoutes);
-// app.use(express.static(path.resolve("./public")))
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// app.use('/api/videos', require('./routes/videoRoutes'));
-// // app.use('/api/videos', videoRoutes);
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-
-
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 
 
 
@@ -54,22 +25,20 @@ app.use("/api", authRoutes);
 app.use(express.static(path.resolve("./public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/videos", require("./routes/videoRoutes"));
+app.use(express.static(path.resolve(__dirname, "..", "frontend", "build"))); 
 
-
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder to the React build folder
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
-
   // Serve index.html on all routes (except API routes)
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "build", "index.html")); // Corrected path
   });
 }
 
+// Default route to send the index.html in development
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 // Socket.I O Connection
 io.on("connection", (socket) => {
   console.log("New client connected");
